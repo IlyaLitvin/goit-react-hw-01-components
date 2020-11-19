a073a87e7a81f05fb51a0e95f5f15629435c27d4
IlyaLitvin <darnon88@gmail.com>
2020-11-19 16:39:19 +0200

    First component is added

---
+++ src/components/Profile/statsItem/StatsItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Profile.css";
+
+const StatsItem = ({ el: [key, value] }) => {
+  return (
+    <li>
+      <span className="label"> {key} </span>
+      <span className="quantity"> {value} </span>
+    </li>
+  );
+};
+export default StatsItem;
